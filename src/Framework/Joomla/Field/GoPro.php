f6d64d5fa8a4a5a0d982d83992126f8cf777e758
Valentín <htmgarcia@gmail.com>
2014-10-23 17:57:24 -0500

    Add class from xml field

--- src/Framework/Joomla/Field/GoPro.php
+++ src/Framework/Joomla/Field/GoPro.php
@@ -9,14 +9,12 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-jimport('joomla.form.formfield');
-
 
 class JFormFieldGoPro extends JFormField
 {
     public function getInput() {
         JHtml::stylesheet( JURI::root() . 'libraries/allediaframework/Framework/assets/css/style.css' );
-        $html = '<div class="ost-alert-gopro">
+        $html = '<div class="ost-alert-gopro ' . $this->class . '">
             <a href="https://www.alledia.com/plans/" class="ost-alert-btn" target="_blank">
             <i class="icon-publish"></i> Go Pro to access more features</a>
             <img src="../libraries/allediaframework/Framework/assets/images/alledia_logo.png" style="width:120px;height:auto;" alt=""/>
@@ -26,6 +24,6 @@ class JFormFieldGoPro extends JFormField
 	}
 
     public function getLabel(){
-        return ' ';
+        return '';
     }
 }
