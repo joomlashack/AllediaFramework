e231d7d24050b8d5882d3e0934bb0e784f244ebd
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-11-05 13:59:40 -0200

    Add a legacy class for Extension
    Adds backwards compatibility with older versions of extensions that still are using this class

---
+++ src/Framework/Extension.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   AllediaFramework
+ * @contact   www.alledia.com, hello@alledia.com
+ * @copyright 2014 Alledia.com, All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace Alledia\Framework;
+
+defined('_JEXEC') or die();
+
+/**
+ * Legacy extension class
+ *
+ * @deprecated Only for backward compatibility. Joomla\Extension\Licensed instead
+ */
+class Extension extends Joomla\Extension\Licensed
+{
+    // @TODO: Remove this class in a future release
+}
