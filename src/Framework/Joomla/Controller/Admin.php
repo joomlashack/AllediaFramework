289c564e243fcf4daecdcbf46b6e16b835387c05
Anderson Grudtner Martins <anderson@ostraining.com>
2015-02-12 12:20:26 -0200

    Adds a Admin Controller

---
+++ src/Framework/Joomla/Controller/Admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package   AllediaFramework
+ * @contact   www.alledia.com, hello@alledia.com
+ * @copyright 2015 Alledia.com, All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace Alledia\Framework\Joomla\Controller;
+
+defined('_JEXEC') or die();
+
+jimport('legacy.controller.admin');
+
+class Admin extends \JControllerAdmin
+{
+
+}
