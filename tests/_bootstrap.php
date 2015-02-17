2b5fdab65ffd93205d9b9cd3b52504d3f6cce9bc
Anderson Grudtner Martins <anderson@ostraining.com>
2015-02-17 11:18:20 -0200

    Remove the AllediaTestFramework dependency

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,8 +4,8 @@ define('ALLEDIAFRAMEWORK_SOURCE_PATH', __DIR__ . '/../src');
 
 $config = json_decode(file_get_contents(__DIR__ . '/config.json'));
 
-// Load the bootstrap file from Alledia Test Framework
-require_once $config->allediaTestFrameworkPath . '/tests/_bootstrap.php';
+// Load the bootstrap file from Alledia Builder
+require_once $config->allediaBuilderPath . '/src/codeception/_bootstrap.php';
 
 // Load Alledia Framework
 require_once ALLEDIAFRAMEWORK_SOURCE_PATH . '/include.php';
