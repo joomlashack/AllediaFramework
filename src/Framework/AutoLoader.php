0e4d33a906cbf13c1556297b33ade0bcf0da6476
Bill Tomczak <bill@ostraining.com>
2018-04-12 17:57:41 -0700

    Update file headers (attribution/email)

--- src/Framework/AutoLoader.php
+++ src/Framework/AutoLoader.php
@@ -2,7 +2,7 @@
 /**
  * @package    AllediaFramework
  * @contact    www.joomlashack.com, help@joomlashack.com
- * @copyright  2016 Alledia.com, All rights reserved
+ * @copyright  2016 Open Source Training, LLC., All rights reserved
  * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * Local copy of the Alledia autoloader
