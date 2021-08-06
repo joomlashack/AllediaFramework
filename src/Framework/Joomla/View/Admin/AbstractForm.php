f5e8657a62ff2497f3408ce876a97e59db90d59a
Bill Tomczak <bill@joomlashack.com>
2021-08-06 14:11:46 -0700

    Provide standard Admin Form view

---
+++ src/Framework/Joomla/View/Admin/AbstractForm.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package   AllediaFramework
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2021 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of AllediaFramework.
+ *
+ * AllediaFramework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * AllediaFramework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllediaFramework.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Alledia\Framework\Joomla\View\Admin;
+
+use Alledia\Framework\Joomla\TraitAllediaView;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\MVC\Model\AdminModel;
+
+defined('_JEXEC') or die();
+
+class AbstractForm extends AbstractBase
+{
+    /**
+     * @var Form
+     */
+    protected $form = null;
+
+    /**
+     * @inheritDoc
+     *
+     */
+    public function setModel($model, $default = false)
+    {
+        $model = parent::setModel($model, $default);
+        if ($model instanceof AdminModel && $default) {
+            $this->form = $model->getForm();
+        }
+
+        return $model;
+    }
+}
