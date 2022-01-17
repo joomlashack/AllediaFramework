2fd6cff61807d2ba789d8b8ae971fcea7270886b
Bill Tomczak <bill@joomlashack.com>
2022-01-17 07:03:21 -0800

    Update file headers

--- src/Framework/Joomla/Extension/Helper.php
+++ src/Framework/Joomla/Extension/Helper.php
@@ -2,7 +2,7 @@
 /**
  * @package   AllediaFramework
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @copyright 2016-2021 Joomlashack.com. All rights reserved
+ * @copyright 2016-2022 Joomlashack.com. All rights reserved
  * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
  *
  * This file is part of AllediaFramework.
