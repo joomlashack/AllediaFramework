3ac143e1f39465449a8a45f4341f81c5b1fbe465
Bill Tomczak <bill@joomlashack.com>
2022-07-19 10:58:44 -0700

    Implement standard Form, Json controllers

---
+++ src/Framework/Joomla/Controller/Form.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @package   AllediaFramework
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2016-2022 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of AllediaFramework.
+ *
+ * AllediaFramework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * AllediaFramework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllediaFramework.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Alledia\Framework\Joomla\Controller;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\Router\Route;
+use Joomla\Input\Input;
+
+defined('_JEXEC') or die();
+
+class Form extends FormController
+{
+    use TraitController;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(
+        $config = [],
+        MVCFactoryInterface $factory = null,
+        ?CMSApplication $app = null,
+        ?Input $input = null
+    ) {
+        parent::__construct($config, $factory, $app, $input);
+
+        $this->customInit();
+    }
+
+
+    /**
+     * @inheritDoc
+     * @throws \Exception
+     */
+    public function batch($model = null)
+    {
+        $this->checkToken();
+
+        $inflector = $this->getStringInflector();
+        $view      = $this->app->input->getCmd('view', $this->default_view);
+
+        if ($inflector->isPlural($view)) {
+            $modelName = $inflector->toSingular($view);
+
+            $model = $this->getModel($modelName, '', []);
+
+            $linkQuery = http_build_query([
+                'option' => $this->app->input->getCmd('option'),
+                'view'   => $view
+            ]);
+            $this->setRedirect(Route::_('index.php?' . $linkQuery . $this->getRedirectToListAppend(), false));
+
+            return parent::batch($model);
+        }
+
+        return null;
+    }
+}
---
+++ src/Framework/Joomla/Controller/Json.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package   OSCampus
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2015-2021 Joomlashack.com. All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of OSCampus.
+ *
+ * OSCampus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OSCampus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OSCampus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Alledia\Framework\Joomla\Controller;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Response\JsonResponse;
+
+defined('_JEXEC') or die();
+
+abstract class Json extends Base
+{
+    /**
+     * @inheritDoc
+     * @throws \Exception`
+     */
+    public function checkToken($method = 'post', $redirect = false)
+    {
+        $valid = parent::checkToken($method, $redirect);
+
+        if (!$valid) {
+            throw new \Exception(Text::_('JINVALID_TOKEN'), 403);
+        }
+
+        return true;
+    }
+
+    /**
+     * Sends a json package to output. All php processing ended to prevent any
+     * plugin processing that might slow things down or waste memory.
+     *
+     * @param ?string|\Throwable $message
+     *
+     * @return void
+     */
+    protected function returnJson($message = null)
+    {
+        $result = new JsonResponse();
+
+        if ($message) {
+            if ($message instanceof \Throwable) {
+                $result->success = false;
+                $result->message = $message->getMessage();
+
+                $result->data = [
+                    'file' => $message->getFile(),
+                    'line' => $message->getLine()
+                ];
+            } else {
+                $result->message = $message;
+            }
+        }
+
+        header('Content-Type: application/json');
+        echo $result;
+
+        jexit();
+    }
+}
