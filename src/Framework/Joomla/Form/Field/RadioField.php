94fb2060c77dc918d7d54bcbcdc8c72ee98f5ef5
Bill Tomczak <bill@joomlashack.com>
2025-11-05 15:22:11 -0800

    Create compatibility layer for Radio FormField

---
+++ src/Framework/Joomla/Form/Field/RadioField.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package   AllediaFramework
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2025 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of AllediaFramework.
+ *
+ * AllediaFramework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * AllediaFramework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllediaFramework.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Alledia\Framework\Joomla\Form\Field;
+
+use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\Version;
+
+// phpcs:disable PSR1.Files.SideEffects
+defined('_JEXEC') or die();
+
+if (Version::MAJOR_VERSION < 4) {
+    FormHelper::loadFieldClass('radio');
+    class_alias(\JFormFieldRadio::class, \Joomla\CMS\Form\Field\RadioField::class);
+}
+// phpcs:enable PSR1.Files.SideEffects
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+
+class RadioField extends \Joomla\CMS\Form\Field\RadioField
+{
+
+}
