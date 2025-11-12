9e3e70f4258a6a99a585eb040b14ec874db9c067
Bill Tomczak <bill@joomlashack.com>
2025-11-12 11:29:08 -0800

    Initial UserHelper compatibility layer

---
+++ src/Framework/Joomla/User/UserHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @package   AllediaFramework
+ * @contact   www.joomlashack.com, help@joomlashack.com
+ * @copyright 2025 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
+ *
+ * This file is part of AllediaFramework.
+ *
+ * AllediaFramework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * AllediaFramework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AllediaFramework.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Alledia\Framework\Joomla\User;
+
+// phpcs:disable PSR1.Files.SideEffects
+defined('_JEXEC') or die();
+
+// phpcs:enable PSR1.Files.SideEffects
+
+class UserHelper extends \Joomla\CMS\User\UserHelper
+{
+    /**
+     * @inheritDoc
+     */
+    public static function getProfile($userId = 0)
+    {
+        $profile = parent::getProfile($userId);
+        if (is_callable([$profile, 'getProperties'])) {
+            $profile = (object)$profile->getProperties();
+        }
+
+        return $profile;
+    }
+}
